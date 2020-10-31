b58b433d29b82dd9275417f37ae3e26d2e592cf2
James Tigert <jimmy.tigert@gmail.com>
2020-10-31 13:17:12 -0500

    Initial commit
    fixed ascii spacing

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)](https://snapcraft.io/chucknorris)
+
+# Chuck Norris Jokes
+
+## Read awesome Chuck Norris jokes from your terminal!
+
+## Install the snap
+
+`sudo snap install chucknorris`
+
+## Run in your terminal: 
+
+`chucknorris`
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="chucknorris",
+    version="1.0",
+    description="Chuck Norris jokes right from your terminal",
+    long_description=open("README.md").read(),
+    license="MIT",
+    packages=["libchucknorris"],
+    scripts=["chucknorris"],
+    package_data={"libchucknorris": ["data/*"]},
+)
